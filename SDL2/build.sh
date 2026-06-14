0571c2226af5a57f239bb17a05b1d4156ea6e2b5
safiworks <116169309+safiworks@users.noreply.github.com>
2026-06-15 01:26:25 +0300

    init

---
+++ SDL2/build.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -eou pipefail
+has_arg() {
+    local needle="$1"
+    shift
+
+    for arg in "$@"; do
+        [[ "$arg" == "$needle" ]] && return 0
+    done
+
+    return 1
+}
+
+if [[ -z "$ARCH" ]]; then
+    echo "Expected ARCH env variable"
+    exit 1
+fi
+
+NO_INSTALL_ONLY=1
+if has_arg --install-only "$@"; then
+    NO_INSTALL_ONLY=0
+fi
+
+CFLAGS="-Wno-incompatible-pointer-types"
+CC="clang -target x86_64-unknown-none" cmake --fresh -S . -B build -G Ninja \
+    -DCMAKE_SYSROOT=$build_sysroot \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="$output_prefix" \
+    -DSDL_LIBC=ON \
+    -DCMAKE_C_FLAGS="$CFLAGS" \
+    --toolchain="$CMAKE_TOOLCHAIN_PATH" \
+    -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+cmake --build build
+cmake --install build --prefix $output_prefix
+echo "Installed raw SDL to $output_prefix"
+echo "Building sdl-safaos"
+
+cd sdl-safaos
+./build.sh $ARCH || exit 1
+cd ..
+
+cp sdl-safaos/out/* $output_prefix/lib/ || exit 1
+echo "Success, Installed in $output_prefix"
---
+++ build.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+if [[ -z "$1" ]]; then
+    echo "Usage: [ARCH] [PORT NAME] [DEST DIR]"
+    exit 1
+fi
+
+if [[ -z "$2" ]]; then
+    echo "Usage: [ARCH] [PORT NAME] [DEST DIR]"
+    exit 1
+fi
+
+if [[ -z "$3" ]]; then
+    echo "Usage: [ARCH] [PORT NAME] [DEST DIR]"
+    exit 1
+fi
+
+set -euo pipefail
+export ARCH=$1
+PORT_NAME=$2
+DEST_DIR=$3
+OG_WORK_DIR=$(pwd)
+
+cd $PORT_NAME
+. ./info
+cd $OG_WORK_DIR
+
+git submodule init
+git submodule update
+
+export build_sysroot="$(pwd)/build"
+export output_prefix="$(pwd)/$DEST_DIR"
+
+export CMAKE_TOOLCHAIN_PATH="$(pwd)/safa-build/Toolchain.txt"
+
+cd safa-build
+./make-sysroot $ARCH $build_sysroot
+cd ..
+
+
+cd $PORT_NAME
+
+DOESNT_EXITS=1
+git clone --filter=blob:none --branch $BRANCH $REPO repo || DOESNT_EXITS=0
+
+cd repo
+if $DOESNT_EXITS; then
+    git checkout "$COMMIT"
+    git checkout -b port
+    git apply ../*.patch
+else
+    echo "Repo already cloned, please delete it to reclone"
+fi
+
+
+../build.sh
