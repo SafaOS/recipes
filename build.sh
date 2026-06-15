d466cef8e391e9e53ce512dd1e155a9131cc24cd
safiworks <116169309+safiworks@users.noreply.github.com>
2026-06-15 23:41:34 +0300

    fix(build.sh): bug with output path and made SDL support install only

--- build.sh
+++ build.sh
@@ -20,6 +20,7 @@ export ARCH=$1
 PORT_NAME=$2
 DEST_DIR=$3
 OG_WORK_DIR=$(pwd)
+export root=$(pwd)
 
 cd $PORT_NAME
 . ./info
@@ -29,7 +30,16 @@ git submodule init
 git submodule update
 
 export build_sysroot="$(pwd)/build"
-export output_prefix="$(pwd)/$DEST_DIR"
+case "$DEST_DIR" in
+    /*)
+        output_prefix="$DEST_DIR"
+        ;;
+    *)
+        output_prefix="$(pwd)/$DEST_DIR"
+        ;;
+esac
+
+export output_prefix
 
 export CMAKE_TOOLCHAIN_PATH="$(pwd)/safa-build/Toolchain.txt"
 
@@ -42,9 +52,15 @@ cd $PORT_NAME
 
 if git clone --filter=blob:none --branch $BRANCH $REPO repo ; then
     cd repo
-    git checkout "$COMMIT"
+    if [[ ! -z "$COMMIT" ]]; then
+        git checkout "$COMMIT"
+    fi
     git checkout -b port
-    git apply ../*.patch
+    if ls ../*.patch 2>/dev/null; then
+        git apply ../*.patch
+    else
+        echo "No patches to apply"
+    fi
 else
     cd repo
     echo "Repo already cloned, please delete it to reclone"
--- ports/SDL2/build.sh
+++ ports/SDL2/build.sh
@@ -22,6 +22,7 @@ if has_arg --install-only "$@"; then
     NO_INSTALL_ONLY=0
 fi
 
+if [[ -z "$INSTALL_ONLY" || "$INSTALL_ONLY" == "0" ]]; then
 CFLAGS="-Wno-incompatible-pointer-types"
 CC="clang -target x86_64-unknown-none" cmake --fresh -S . -B build -G Ninja \
     -DCMAKE_SYSROOT=$build_sysroot \
@@ -31,8 +32,8 @@ CC="clang -target x86_64-unknown-none" cmake --fresh -S . -B build -G Ninja \
     -DCMAKE_C_FLAGS="$CFLAGS" \
     --toolchain="$CMAKE_TOOLCHAIN_PATH" \
     -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-
-cmake --build build
+    cmake --build build
+fi
 cmake --install build --prefix $output_prefix
 echo "Installed raw SDL to $output_prefix"
 echo "Building sdl-safaos"
