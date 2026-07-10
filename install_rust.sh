fff8677a7b2695e0e1d0878a54ea5766e56d986b
safiworks <116169309+safiworks@users.noreply.github.com>
2026-07-10 20:55:25 +0300

    fix(install_rust):

--- install_rust.sh
+++ install_rust.sh
@@ -1,5 +1,5 @@
 set -eou pipefail
 git clone https://github.com/SafaOS/SafaOS --branch GUI
 cd SafaOS
-./helper.sh install-toolchain --arch x86_64
-./helper.sh install-toolchain -arch aarch64
+./helper.sh init --arch x86_64
+./helper.sh init --arch aarch64
