6bb04f85d4d58dec7629b1acf332a959214b8061
safiworks <116169309+safiworks@users.noreply.github.com>
2026-07-28 23:58:16 +0300

    fix(install_rust.sh): default to main

--- install_rust.sh
+++ install_rust.sh
@@ -1,5 +1,5 @@
 set -eou pipefail
-git clone https://github.com/SafaOS/SafaOS --branch GUI
+git clone https://github.com/SafaOS/SafaOS
 cd SafaOS
 ./helper.sh init --arch x86_64
 ./helper.sh init --arch aarch64
